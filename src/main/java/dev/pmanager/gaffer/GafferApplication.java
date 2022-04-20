f13b19bd4e7ee4e6bfb9186807d2c2edbbb8280f
Thubelihle Sibanda <thubelihlesibanda716@gmail.com>
2022-04-20 12:51:15 +0100

    Initial commit

---
+++ src/main/java/dev/pmanager/gaffer/GafferApplication.java
@@ -0,0 +1,13 @@
+package dev.pmanager.gaffer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GafferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GafferApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/pmanager/gaffer/GafferApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.pmanager.gaffer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GafferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
